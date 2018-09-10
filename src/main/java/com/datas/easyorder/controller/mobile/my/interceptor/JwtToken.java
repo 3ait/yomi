421eaf1586348f5ebdef58b01dc8c5c4f27fee68
Leo <leo@3a.co.nz>
2018-09-11 11:48:43 +1200

    udpate

--- src/main/java/com/datas/easyorder/controller/mobile/my/interceptor/JwtToken.java
+++ src/main/java/com/datas/easyorder/controller/mobile/my/interceptor/JwtToken.java
@@ -17,7 +17,7 @@ public class JwtToken {
 	
 	
 	/** token秘钥，请勿泄露，请勿随便修改 backups:JKKLJOoasdlfj */  
-	public static final String SECRET = "uLtiMAtelifeDatas";  
+	public static final String SECRET = "tokenSecretyomi";  
 	/** token 过期时间: 10天 */  
 	public static final int calendarField = Calendar.DATE;  
 	public static final int calendarInterval = 30;  
